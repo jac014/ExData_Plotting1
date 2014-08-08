78342a64876d9263527d98707ac91eed5e894b6a
jac014 <jon@shark.local>
2014-08-07 22:13:41 -0400

    plot1 bg removed

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,s
 powercons <- subset(power, Date == "2/2/2007" | Date == "1/2/2007")
 rm(powerall)
 powercons$Date <- as.Date(powercons$Date, format='%d/%m/%Y')
-hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red", cex.lab = .75, cex.main = .75, cex.axis = .75)
+par(bg=NA)
+hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", bg = NA ,col = "red", cex.lab = .75, cex.main = .75, cex.axis = .75)
 dev.copy(png, file = "plot1.png", width=480, height=480) ## Copy  plot to a PNG file
 dev.off()
