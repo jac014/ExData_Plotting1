f1e6bbe20f239fed88d946a98067a6bf7b1c6f9e
jac014 <jon@shark.local>
2014-08-08 17:45:57 -0400

    plot3 added

---
+++ plot3.R
@@ -0,0 +1,14 @@
+##Load data and filter
+powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,sep = ";",na.strings = "?", colClasses = c("character","character", rep("numeric",7)))
+powercons <- subset(powerall, Date == "2/2/2007" | Date == "1/2/2007")
+rm(powerall)
+##Set date format
+powercons$Date <- strptime(paste(powercons$Date,powercons$Time), "%d/%m/%Y %H:%M:%S")
+##Plot and output
+par(bg=NA)
+plot(x = powercons$Date, y = powercons$Sub_metering_1, type = "l", ylab = "Energy sub metering",cex.lab = .75, cex.axis = .75)
+lines(x= powercons$Date, y = powercons$Sub_metering_2, type="l", col="red")
+lines(x= powercons$Date, y = powercons$Sub_metering_3, type="l", col="blue")
+legend("topright", lty = "solid", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = .75)
+dev.copy(png, file = "plot2.png", width=480, height=480)
+dev.off()
