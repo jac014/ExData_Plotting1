d7a972d879f6ba5acbe1a8683eb057780dd2ea30
jac014 <jon@shark.local>
2014-08-07 22:46:32 -0400

    plot2 orig

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-##Load data and filter on dates
+##Load data and filter
 powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,sep = ";",na.strings = "?", colClasses = c("character","character", rep("numeric",7)))
 powercons <- subset(power, Date == "2/2/2007" | Date == "1/2/2007")
 rm(powerall)
@@ -6,6 +6,6 @@ rm(powerall)
 powercons$Date <- as.Date(powercons$Date, format='%d/%m/%Y')
 ##Plot and output
 par(bg=NA)
-hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", bg = NA ,col = "red", cex.lab = .75, cex.main = .75, cex.axis = .75)
+hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power",col = "red", cex.lab = .75, cex.main = .80, cex.axis = .75)
 dev.copy(png, file = "plot1.png", width=480, height=480)
 dev.off()
---
+++ plot2.R
@@ -0,0 +1,11 @@
+##Load data and filter
+powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,sep = ";",na.strings = "?", colClasses = c("character","character", rep("numeric",7)))
+powercons <- subset(powerall, Date == "2/2/2007" | Date == "1/2/2007")
+rm(powerall)
+##Set date format
+powercons$Date <- strptime(paste(powercons$Date,powercons$Time), "%d/%m/%Y %H:%M:%S")
+##Plot and output
+par(bg=NA)
+plot(powercons$Date, powercons$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "",cex.lab = .75, cex.axis = .75)
+dev.copy(png, file = "plot2.png", width=480, height=480)
+dev.off()
