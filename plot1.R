ea069e7bef5aec4856e9726699cb3555dbcd8cec
jac014 <jon@shark.local>
2014-08-07 22:17:57 -0400

    add comments, larger main

--- plot1.R
+++ plot1.R
@@ -1,8 +1,11 @@
+##Load data and filter on dates
 powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,sep = ";",na.strings = "?", colClasses = c("character","character", rep("numeric",7)))
 powercons <- subset(power, Date == "2/2/2007" | Date == "1/2/2007")
 rm(powerall)
+##Set date format
 powercons$Date <- as.Date(powercons$Date, format='%d/%m/%Y')
+##Plot and output
 par(bg=NA)
 hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", bg = NA ,col = "red", cex.lab = .75, cex.main = .75, cex.axis = .75)
-dev.copy(png, file = "plot1.png", width=480, height=480) ## Copy  plot to a PNG file
+dev.copy(png, file = "plot1.png", width=480, height=480)
 dev.off()
