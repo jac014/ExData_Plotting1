82af4061676a423c8b3e634968911930c0874cb7
jac014 <jon@shark.local>
2014-08-07 22:00:51 -0400

    plot1 added

---
+++ plot1.R
@@ -0,0 +1,7 @@
+powerall<- read.table(file = "household_power_consumption.txt", header = TRUE ,sep = ";",na.strings = "?", colClasses = c("character","character", rep("numeric",7)))
+powercons <- subset(power, Date == "2/2/2007" | Date == "1/2/2007")
+rm(powerall)
+powercons$Date <- as.Date(powercons$Date, format='%d/%m/%Y')
+hist(powercons$Global_active_power, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
+dev.copy(png, file = "plot1.png", width=480, height=480) ## Copy  plot to a PNG file
+dev.off()
